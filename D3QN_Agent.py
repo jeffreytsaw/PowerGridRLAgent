98b44bc299f3b861f60a8b141210b124b06d5039
jeffreytsaw <31784874+jeffreytsaw@users.noreply.github.com>
2020-08-16 19:00:00 +0800

    Added D3QN network and agent

---
+++ D3QN_Agent.py
@@ -0,0 +1,313 @@
+import os
+import math
+import numpy as np
+import tensorflow as tf
+
+
+from grid2op.Agent import AgentWithConverter
+from grid2op.Converter import IdToAct
+from prioritized_replay_buffer import PrioritizedReplayBuffer
+
+from D3QN import D3QNet
+
+# Class for a Grid2Op Agent that uses the network defined in DQNet.py to estimate
+# Q values
+
+class D3QNetAgent(AgentWithConverter):
+    def __init__(self, 
+                 observation_space,
+                 action_space,
+                 num_frames = 4,
+                 batch_size = 32,
+                 learning_rate = 1e-5,
+                 learning_rate_decay_steps = 10000,
+                 learning_rate_decay_rate = 0.95,
+                 discount_factor = 0.95,
+                 tau = 1e-2,
+                 lam = 1,
+                 per_size = 50000,
+                 per_alpha = 0.6,
+                 per_beta = 0.4,
+                 per_anneal_rate = 1.5e6,
+                 epsilon = 0.99,
+                 decay_epsilon = 1024*32,
+                 final_epsilon = 0.0001):
+        
+        # initializes AgentWithConverter class to handle action conversiions
+        AgentWithConverter.__init__(self, action_space,
+                                    action_space_converter=IdToAct)
+        
+        self.obs_space = observation_space
+        self.act_space = action_space
+        self.num_frames = num_frames
+        
+        self.batch_size = batch_size
+        self.lr = learning_rate
+        self.lr_decay_steps = learning_rate_decay_steps
+        self.lr_decay_rate = learning_rate_decay_rate
+        self.gamma = discount_factor
+        self.lam = lam
+        self.tau = tau
+        # epsilon is the degree of exploraation
+        self.initial_epsilon = epsilon
+        # Adaptive epsilon decay constants
+        self.decay_epsilon = decay_epsilon
+        self.final_epsilon = final_epsilon
+        
+        #PER data
+        self.buff_size = per_size
+        self.alpha = per_alpha
+        self.beta = per_beta
+        self.anneal = per_anneal_rate
+        
+        self.observation_size = self.obs_space.size_obs()
+        self.action_size = self.act_space.size()
+        
+        self.d3qn = D3QNet(self.action_size,
+                         self.observation_size,
+                         self.num_frames,
+                         self.lr,
+                         self.lr_decay_steps,
+                         self.lr_decay_rate,
+                         self.batch_size,
+                         self.gamma,
+                         self.tau)
+        
+        #State variables
+        self.obs = None
+        self.done = None
+        self.epsilon = self.initial_epsilon
+        
+        self.state = []
+        self.frames = []
+        self.next_frames = []
+        self.replay_buffer = PrioritizedReplayBuffer(self.buff_size, self.alpha,
+                                                    self.beta, self.anneal)
+        
+        return
+    
+    ## Helper Functions
+    
+    # Adds to current frame buffer, enforfes length to num_frames
+    def update_curr_frame(self, obs):
+        self.frames.append(obs.copy())
+        if (len(self.frames) > self.num_frames):
+            self.frames.pop(0)
+        return
+    # Adds next frame to next frame buffer, enforces length to num_frames
+    def update_next_frame(self, next_obs):
+        self.next_frames.append(next_obs.copy())
+        if (len(self.next_frames) > self.num_frames):
+            self.next_frames.pop(0)
+        return
+    
+    # Adaptive epsilon decay determines next epsilon based off number of steps
+    # completed and curren epsilon
+    def set_next_epsilon(self, current_step):
+        ada_div = self.decay_epsilon / 10.0
+        step_off = current_step + ada_div
+        ada_eps = self.initial_epsilon * -math.log10((step_off + 1) / 
+                                            (self.decay_epsilon + ada_div))
+        ada_eps_up_clip = min(self.initial_epsilon, ada_eps)
+        ada_eps_low_clip = max(self.final_epsilon, ada_eps_up_clip)
+        self.epsilon = ada_eps_low_clip
+        return
+    
+    ## Agent Interface    
+    
+    #Adapted from l2rpn-baselines from RTE-France
+    # Vectorizes observations from grid2op environment for neural network uses
+    def convert_obs(self, obs):
+        li_vect=  []
+        for el in obs.attr_list_vect:
+            v = obs._get_array_from_attr_name(el).astype(np.float32)
+            v_fix = np.nan_to_num(v)
+            v_norm = np.linalg.norm(v_fix)
+            if v_norm > 1e6:
+                v_res = (v_fix / v_norm) * 10.0
+            else:
+                v_res = v_fix
+            li_vect.append(v_res)
+        return np.concatenate(li_vect)
+    
+    # converts encoded action number to action used to interact with grid2op
+    # environment
+    def convert_act(self, encoded_act):
+        return super().convert_act(encoded_act)
+    
+    # Required for agent evaluation
+    # Returns random action or best action as estimated by Q network based on
+    # exploration parameter (espilon)
+    def my_act(self, state, reward, done):
+        if (len(self.frames) < self.num_frames): return 0 #do nothing
+        random_act = random.randint(0, self.action_size)
+        self.update_curr_frame(state)
+        qnet_act, _ = self.dqn.model_action(np.array(self.frames))
+        if (np.random.rand(1) < self.epsilon):
+            return random_act 
+        else:
+            return qnet_act
+
+    
+    ## Training Loop
+    def learn(self,
+              env,
+              num_epochs,
+              num_steps,
+              soft_update_freq = 250,
+              hard_update_freq = 1000):
+        
+        #pre-training to fill buffer
+        
+        print("Starting Pretraining...\n")
+        self.done = True
+        # Plays random moves and saves the resulting (s, a, r, s', d) pair to 
+        # replay buffer. Resets environment when done and continues
+        while (len(self.replay_buffer) < self.buff_size):
+            if (self.done):
+                # reset environment and state parameters
+                new_env = env.reset()
+                self.frames = []
+                self.next_frames = []
+                self.done = False
+                self.obs = new_env
+                self.state = self.convert_obs(self.obs)
+                
+            self.update_curr_frame(self.state)
+           
+            # action is random 
+            encoded_act = np.random.randint(0, self.action_size)
+            act = self.convert_act(encoded_act)
+            new_obs, reward, self.done, info = env.step(act)
+            
+            gplay_reward = info['rewards']['gameplay']
+            adj_reward = self.lam*reward + (1-self.lam)*gplay_reward
+                        
+            new_state = self.convert_obs(new_obs)
+            self.update_next_frame(new_state)
+            
+            # only add to buffer if num_frames states are seen
+            if (len(self.frames) == self.num_frames and 
+                len(self.next_frames) == self.num_frames):
+                    
+                    agg_state = np.array(self.frames)
+                    agg_next_state = np.array(self.next_frames)
+                    #(s,a,r,s',d) pair
+                    self.replay_buffer.add(agg_state,
+                                           encoded_act, adj_reward,
+                                           agg_next_state,
+                                           self.done)
+                    
+            self.obs = new_obs
+            self.state = new_state
+    
+        
+        epoch = 0 # number of complete runs through environment
+        total_steps = 0 # total number of training steps across all epochs
+        
+        losses = [] # losses[i] is loss from dqn at total_step i
+        avg_losses = [] # avg_losses[i] is avg loss during training during epcoh i
+        net_reward = [] #net_reward[i] is total reward during epoch i
+        alive = [] # alive[i] is number of steps survived for at epoch i
+        
+        print("Starting training...\n")
+        
+        # Trains a minimum of num_steps or num_epochs
+        while (total_steps < num_steps or epoch < num_epochs):
+            
+            total_reward = 0
+            curr_steps = 0
+            total_loss = []
+            
+            # Reset state parameters
+            self.frames = []
+            self.next_frames = []
+            self.done = False
+            self.obs = env.reset()
+            self.state = self.convert_obs(self.obs)
+            
+            # continues until failure 
+            while (not self.done):
+                
+                self.update_curr_frame(self.state)
+            
+                # Determine action 
+                if (len(self.frames) < self.num_frames):
+                    enc_act = 0 # do nothing
+                elif (np.random.rand(1) < self.epsilon):
+                    enc_act = np.random.randint(0, self.action_size)
+                else:
+                    input = np.array(self.frames)
+                    enc_act, _ = self.d3qn.model_action(input)
+                
+                # converts action and steps in environment
+                act = self.convert_act(enc_act)
+                new_obs, reward, self.done, info = env.step(act)
+                
+                gplay_reward = info['rewards']['gameplay']
+                adj_reward = self.lam*reward + (1-self.lam)*gplay_reward
+                
+                new_state = self.convert_obs(new_obs)
+                # updates next_state frame
+                self.update_next_frame(new_state)
+                
+                if (len(self.frames) == self.num_frames and
+                    len(self.next_frames) == self.num_frames):
+                        
+                    agg_state = np.array(self.frames)
+                    agg_next_state = np.array(self.next_frames)
+                    # Adds (s,a,r,s',d) tuple to replay buffer
+                    self.replay_buffer.add(agg_state,
+                                           encoded_act, adj_reward,
+                                           agg_next_state,
+                                           self.done)
+                # finds the next epsilon 
+                self.set_next_epsilon(total_steps)
+                
+                # samples a batch_size number of experience samples from replay
+                # buffer
+                (s_batch, a_batch, r_batch, s_next_batch, d_batch, 
+                w_batch, ind_batch)= (self.replay_buffer.sample(self.batch_size, 
+                                                                total_steps))
+               
+                # updates network estimates based on replay
+                loss = self.d3qn.train_on_minibatch(s_batch, a_batch, r_batch, 
+                                        s_next_batch, d_batch, w_batch)
+                                        
+                priorities = self.d3qn.prio
+                self.replay_buffer.update_priorities(ind_batch, priorities)
+                
+                # periodically hard updates the network 
+                if (total_steps % hard_update_freq):
+                    self.d3qn.hard_update_target_network()
+                
+                # periodically soft update the network    
+                elif (total_steps % soft_update_freq):
+                    self.d3qn.soft_update_target_network()
+                
+                # update state variables
+                self.obs = new_obs
+                self.state = new_state
+                
+                # increase steps, updates metrics
+                curr_steps += 1
+                total_steps += 1
+                total_reward += reward
+                losses.append(loss)
+                total_loss.append(loss)
+            
+            # updates metrics throughout epoch
+            alive.append(curr_steps)
+            net_reward.append(total_reward)            
+            avg_losses.append(np.average(np.array(total_loss)))
+            
+            epoch += 1
+            # sanity check to ensure it's working
+            if (epoch % 100 == 0):
+                print("Completed epoch {}".format(epoch))
+                print("Total steps: {}".format(total_steps))
+        
+        return (epoch, total_steps, losses, avg_losses, net_reward, alive)
+            
+                
+        
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os 
+import pickle
 
 import grid2op
 import tensorflow as tf
@@ -7,34 +8,75 @@ import numpy as np
 
 from DQNet_Agent import DQNetAgent
 from RandomAgent import RandomAgent
-from grid2op.Reward import L2RPNReward
+from D3QN_Agent import D3QNetAgent
+from grid2op.Reward import L2RPNReward, GameplayReward
 
 
 ## Hyperparameters
 num_frames = 4
 batch_size = 32
+
+buffer_size = 70000
+per_alpha = 0.6
+per_beta = 0.4
+per_anneal_steps = int(1.5e6)
+
 lr = 1e-5
 lr_decay_steps = 100000/4
 lr_decay_rate = 0.95
+
 discount_factor = 0.99
 tau = 1e-2
-buffer_size = 70000
+lam = 0.7
+
 epsilon = 0.99
 decay_epsilon = 100000
 final_epsilon = 0.01
 
 num_epochs = 10000
-num_steps = 1500000
+num_steps = int(1.7e6)
+
 soft_update_freq = -1
 hard_update_freq = 1000
 
 
 env_name = "rte_case14_realistic"
 
-env = grid2op.make(env_name, reward_class=L2RPNReward)
+env = grid2op.make(env_name, reward_class=L2RPNReward,
+                    other_rewards={"gameplay": GameplayReward})
 
-## Wrapper Train Function
-def train(env, num_frames, batch_size, buffer_size,
+##  Wrapper Train for D3QN
+def train_d3qn(env, num_frames, batch_size, 
+            buffer_size, per_alpha, per_beta, per_anneal_steps,
+            lr, lr_decay_steps, lr_decay_rate, 
+            epsilon, decay_epsilon, final_epsilon,
+            discount_factor, tau):
+                
+        D3QAgent = D3QNetAgent(env.observation_space,
+                             env.action_space,
+                             num_frames,
+                             batch_size,
+                             lr, 
+                             lr_decay_steps,
+                             lr_decay_rate,
+                             discount_factor,
+                             tau,
+                             lam,
+                             buffer_size,
+                             per_alpha,
+                             per_beta,
+                             per_anneal_steps,
+                             epsilon,
+                             decay_epsilon,
+                             final_epsilon)
+        epochs, total_steps, losses, avg_losses, net_reward, alive = (
+        D3QAgent.learn(env, num_epochs, num_steps,
+                     soft_update_freq, hard_update_freq))
+                     
+        return epochs, total_steps, losses, avg_losses, net_reward, alive
+        
+## Wrapper Train for DQN 
+def train_dqn(env, num_frames, batch_size, buffer_size,
           lr, lr_decay_steps, lr_decay_rate, 
           epsilon, decay_epsilon, final_epsilon,
           discount_factor, tau):
@@ -68,32 +110,116 @@ def train_random(env, num_epochs):
 
 # Runs custom agent
 epochs, total_steps, losses, avg_losses, net_reward, alive = (
-            train(env, num_frames, batch_size, buffer_size, lr, lr_decay_steps,
-                    lr_decay_rate, epsilon, decay_epsilon, final_epsilon, 
+            train_d3qn(env, num_frames, batch_size, buffer_size, 
+                    per_alpha, per_beta, per_anneal_steps, 
+                    lr, lr_decay_steps, lr_decay_rate, 
+                    epsilon, decay_epsilon, final_epsilon, 
                     discount_factor, tau))
+
 
 print("Running Baseline...")
 # runs random agent
+'''
 r_total_steps, r_losses, r_avg_losses, r_net_reward, r_alive = (
     train_random(env, epochs))
+'''
+
+with open('Baseline_data/alive', 'rb') as f:
+    r_alive = pickle.load(f)
+
+with open('Baseline_data/net_reward', 'rb') as f:
+    r_net_reward = pickle.load(f)
     
+#D3 DATA
+'''
+with open('D3QN_data/alive', 'rb') as f:
+    d3_alive = pickle.load(f)
+
+with open('D3QN_data/avg_losses', 'rb') as f:
+    d3_avg_losses = pickle.load(f)
+
+with open('D3QN_data/epochs', 'rb') as f:
+    d3_epochs = pickle.load(f)
+
+with open('D3QN_data/losses', 'rb') as f:
+    d3_losses = pickle.load(f)
+
+with open('D3QN_data/net_reward', 'rb') as f:
+    d3_net_reward = pickle.load(f)
+
+with open('D3QN_data/total_steps', 'rb') as f:
+    d3_total_steps = pickle.load(f)
+'''
+
+#DQ DATA  
+with open('DQN_data/alive', 'rb') as f:
+    dq_alive = pickle.load(f)
+
+with open('DQN_data/avg_losses', 'rb') as f:
+    dq_avg_losses = pickle.load(f)
+
+with open('DQN_data/epochs', 'rb') as f:
+    dq_epochs = pickle.load(f)
+
+with open('DQN_data/losses', 'rb') as f:
+    dq_losses = pickle.load(f)
+
+with open('DQN_data/net_reward', 'rb') as f:
+    dq_net_reward = pickle.load(f)
+
+with open('DQN_data/total_steps', 'rb') as f:
+    dq_total_steps = pickle.load(f)
+
+# vector adjustments
+epochs = max(dq_epochs, d3_epochs)
+while (len(dq_net_reward) < epochs):
+    dq_net_reward.append(np.max(np.array(dq_net_reward)))
+
+while (len(dq_alive) < epochs):
+    dq_alive.append(np.max(np.array(dq_alive)))
+
+while (len(r_alive) < epochs):
+    r_alive.append(np.max(np.array(r_alive)))
+
+while (len(r_net_reward) < epochs):
+    r_net_reward.append(np.max(np.array(r_net_reward)))
+
+while (len(dq_avg_losses) < epochs):
+    dq_avg_losses.append(np.min(np.array(dq_avg_losses)))
+
+
 
 print('Plotting...')
-plt.figure(5)
+al = plt.figure(5)
 # survival time for each epoch in custom agent (red line) vs random agent (blue line)
-plt.plot(np.arange(epochs), alive, 'r-', np.arange(epochs), r_alive, 'b-')
+plt.plot(np.arange(epochs), alive, 'r-', np.arange(epochs), r_alive, 'b-',
+        np.arange(epochs), dq_alive, 'g-')
+al.suptitle('Survival Duration per Epoch')
+plt.xlabel('Epoch')
+plt.ylabel('Survival Duration')
 
-plt.figure(6)
+re = plt.figure(6)
 # total reward in each epoch in custom agent (red line) vs random agent (blue line)
 plt.plot(np.arange(epochs), net_reward, 'r-', 
-            np.arange(epochs), r_net_reward, 'b-')
+            np.arange(epochs), r_net_reward, 'b-',
+            np.arange(epochs), dq_net_reward, 'g-')
+re.suptitle('Reward per Epoch')
+plt.xlabel('Epoch')
+plt.ylabel('Net Reward')
 
-plt.figure(7)
+avl = plt.figure(7)
 # average losses during each epoch 
-plt.plot(np.arange(epochs), avg_losses, 'r-')
+plt.plot(np.arange(epochs), avg_losses, 'r-', 
+         np.arange(epochs), dq_avg_losses, 'g-')
+avl.suptitle('Average Losses per Epoch')
+plt.xlabel('Epoch')
+plt.ylabel('Average Loss')
 
-plt.figure(8)
+tl = plt.figure(8)
 # total losses during training
 plt.plot(np.arange(total_steps), losses, 'r-')
-plt.show()
+tl.suptitle('Total Loss per Step')
+plt.xlabel('Step')
+plt.ylabel('Training Loss')
 
+plt.show()
